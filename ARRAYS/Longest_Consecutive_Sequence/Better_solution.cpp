abfce71754095556d6542ad494f5894fcbe44774
Abhijeet Mishra <96250691+abhijeet1003@users.noreply.github.com>
2023-10-05 01:38:05 +0530

    Update Better_solution.cpp

--- ARRAYS/Longest_Consecutive_Sequence/Better_solution.cpp
+++ ARRAYS/Longest_Consecutive_Sequence/Better_solution.cpp
@@ -4,7 +4,8 @@
 // QUESTIONS
 
 /*
-Given an array of positive integers. Find the length of the longest sub-sequence such that elements in the subsequence are consecutive integers, the consecutive numbers can be in any order.
+Given an array of positive integers. Find the length of the longest sub-sequence such that 
+elements in the subsequence are consecutive integers, the consecutive numbers can be in any order.
 */
 
 
